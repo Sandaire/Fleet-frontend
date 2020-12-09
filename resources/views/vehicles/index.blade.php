fff88c348a39b1c2ca30fbf94bd6d5b2c66b2cc1
Sandyna Sandaire Jerome <sandynasj@icloud.com>
2020-12-09 18:51:39 -0500

    Add dropdows list

--- resources/views/vehicles/index.blade.php
+++ resources/views/vehicles/index.blade.php
@@ -16,9 +16,53 @@
                 <li class="nav-item"><a data-toggle="tab" href="#tab-eg7-5" class="nav-link"> Out of service</a></li>
 
             </ul>
-        </div>
-        <div class="card-body">
-            <div class="tab-content">
+		</div>
+        <div class="card-body" >
+			<div class="btn-group btn btn-outline-success" style="font-size:large; height:40px; width:240px"> 
+				<button class="btn btn-lg dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+					All Vehicles
+				</button>
+				<div class="dropdown-menu">
+					<a class="dropdown-item active" href="#">All Vehicles</a>
+					<a class="dropdown-item" href="#">BB-15</a>
+					<a class="dropdown-item" href="#">CJ-01</a>
+					<a class="dropdown-item" href="#">CJ-28</a>
+					<div class="dropdown-divider"></div>
+					<a class="dropdown-item" href="#">DN-529</a>
+				</div>
+			</div>
+			<div class="btn-group btn btn-outline-success" style="font-size:14px; margin-left:10px; height:40px; width:240px">
+				<button class="btn btn-lg dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+					All Types
+				</button>
+				<div class="dropdown-menu">
+					<a class="dropdown-item active" href="#">All Types</a>
+					<a class="dropdown-item" href="#">Truck</a>
+					<a class="dropdown-item" href="#">Car</a>
+					<div class="dropdown-divider"></div>
+					<a class="dropdown-item" href="#">Bus</a>
+				</div>
+			</div>
+			<div class="btn-group btn btn-outline-success" style="font-size:large;margin-left:10px; height:40px; width:240px">
+				<button class="btn btn-lg dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+					All Affiliate
+				</button>
+				<div class="dropdown-menu">
+					<a class="dropdown-item active" href="#">All Affiliate</a>
+					<a class="dropdown-item" href="#">Dinasa</a>
+					<a class="dropdown-item" href="#">Sodigaz</a>
+					<div class="dropdown-divider"></div>
+					<a class="dropdown-item" href="#">Aviation</a>
+				</div>
+			</div>
+			<div class="btn-group btn btn-outline-success" style="font-size:large;margin-left:30px">
+				<button class="btn btn-lg dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+					Clear
+				</button>
+			</div>
+			</br></br></br>
+
+            <div class="tab-content ">
                 <div class="tab-pane active" id="tab-eg7-0" role="tabpanel">
 					@include('tabbar.vehicleViewsAll')
                 </div>
